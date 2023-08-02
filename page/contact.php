ab8c03456870e23dcc49463ebdcc47630ebb0790
Noorzahara <izra.aliya28@gmail.com>
2023-08-02 14:34:56 +0700

    Your commit message here

---
+++ page/contact.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" href="css/contact.css">
+
+<div class="container">
+  <h1>Contact</h1>
+  <div class="content">
+    <img src="img/contact.png" alt="about">
+    <div class="contactContent">
+      <div>
+        <img src="img/icons/fa-phone.svg" height="25" alt="" srcset="">
+        <p>+62 812 3456 7891</p>
+      </div>
+      <div>
+        <img src="img/icons/fa-email.svg" height="25" alt="" srcset="">
+        <p>@cierrybutik@gmail.com</p>
+      </div>
+      <div>
+        <img src="img/icons/fa-instagram.svg" height="25" alt="" srcset="">
+        <p>@Cierry Butik Muslim</p>
+      </div>
+      <div>
+        <img src="img/icons/fa-twitter.svg" height="25" alt="" srcset="">
+        <p>@Cierry Butik Muslim</p>
+      </div>
+      <div>
+        <img src="img/icons/fa-facebook.svg" height="25" alt="" srcset="">
+        <p>@Cierry Butik Muslim</p>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ page/footer.php
@@ -0,0 +1,43 @@
+<footer>
+  <div class="information">
+    <h2>Information</h2>
+    <div class="item">
+      <a href="#">About</a>
+      <a href="#">Location</a>
+      <a href="#">Contact</a>
+      <a href="#">eCatalog</a>
+    </div>
+  </div>
+  <div class="services">
+    <h2>Customer Service</h2>
+    <div class="item">
+      <p>Cierry Butik Muslim</p>
+      <p>Phone: 0812 3456 7891</p>
+      <p>Email: cierrybutik@gmail.com</p>
+    </div>
+  </div>
+  <div class="socialMedia">
+    <h2>Social Media</h2>
+    <div class="item">
+      <a href="#">
+        <img src="img/icons/fa-instagram.svg" alt="" width="25" height="25">
+      </a>
+      <a href="#">
+        <img src="img/icons/fa-twitter.svg" alt="" width="25" height="25">
+      </a>
+      <a href="#">
+        <img src="img/icons/fa-facebook.svg" alt="" width="25" height="25">
+      </a>
+    </div>
+  </div>
+  <div class="newsletter">
+    <h2>Newsletter</h2>
+    <div class="item">
+      <p>Subscribe to receive<br>updates, access, to exclusive deal, and more.</p>
+      <div>
+        <input type="email" placeholder="Enter your Email here">
+        <button>SUBSCRIBE</button>
+      </div>
+    </div>
+  </div>
+</footer>
